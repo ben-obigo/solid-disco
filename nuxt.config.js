52e085841112c97e9bd54b110a0b0e867c76fd19
ben-obigo <benjamin.kim@obigo.com>
2020-11-18 01:25:26 +0900

    ci: add gh-pages deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,11 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  // Router base
+  router: {
+    base: '/solid-disco/'
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'solid-disco',
